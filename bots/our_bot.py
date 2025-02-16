70d10b7d7a5674bc700bb1fcd9149533cb4f3e7b
andrewly7561 <andrewtl2006@gmail.com>
2025-02-15 23:56:56 -0500

    threat level and farming

--- bots/our_bot.py
+++ bots/our_bot.py
@@ -6,10 +6,19 @@ from src.game_constants import Team, Tile, GameConstants, Direction, BuildingTyp
 from src.units import Unit
 from src.buildings import Building
 
+def calc_threat_level(rc: RobotController, enemy_ids,home_x,home_y):
+        threat_level = 0
+        for unit_id in enemy_ids:
+            unit = rc.get_unit_from_id(unit_id)
+            if (rc.chebyshev_distance_valid(unit.x, unit.y, home_x, home_y, 10)):
+                threat_level += unit.cost
+            else:
+                threat_level += 1/2 * unit.cost
+
 class BotPlayer(Player):
     def __init__(self, map: Map):
         self.map = map
-    
+
     def play_turn(self, rc: RobotController):
         
         team = rc.get_ally_team()
@@ -20,6 +29,11 @@ class BotPlayer(Player):
             if building.type == BuildingType.MAIN_CASTLE:
                 ally_castle_id = rc.get_id_from_building(building)[1]
                 break
+        
+        ally_units = rc.get_units(team)
+
+        home_x = rc.get_building_from_id(ally_castle_id).x
+        home_y = rc.get_building_from_id(ally_castle_id).y
 
         enemy = rc.get_enemy_team()
         enemy_castle_id = -1
@@ -34,6 +48,15 @@ class BotPlayer(Player):
         if enemy_castle is None: 
             return
         
+         # if threat level is above a certain level build catapults
+        if (calc_threat_level(rc, rc.get_unit_ids(enemy),home_x,home_y) - rc.get_balance >= 1/2 * rc.get_balance):
+            if rc.can_spawn_unit(UnitType.CATAPULT, ally_castle_id):
+                rc.spawn_unit(UnitType.CATAPULT, ally_castle_id)
+        else:
+            # Otherwise build farms in squares +-two left and right up and down from home base.
+            for i in [2,-2]:
+                for j in [2,-2]:
+                    if (rc.can_build_building(BuildingType.FARM1,home_x+i,home_y+j)):
+                        rc.build_building(BuildingType.FARM1,home_x+i,home_y)
 
-        
         return
